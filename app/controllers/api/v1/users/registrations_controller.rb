f178d4e59861b5319e89edc587461a3501bdefb2
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-09 21:22:00 +0100

    Create user session and registration controllers, update application controller and routes

---
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    render_resource(resource)
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
       resources :users do
         resources :boats do
           resources :reservations
