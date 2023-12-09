052c7b245339ca6387b711c464b69b2f7ebd24ce
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-09 21:34:21 +0100

    Setup devise-jwt

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :boats, dependent: :destroy
 
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable,
+         jwt_revocation_strategy: self
+
   validates :name, presence: true, uniqueness: true
 end
