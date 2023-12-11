6f7a66606a186c0f3e93cb30083cbb0fc115e057
Ulises <ulisescappa@gmail.com>
2023-12-11 20:26:25 -0300

    Fix linters errors

--- app/controllers/api/v1/boats_controller.rb
+++ app/controllers/api/v1/boats_controller.rb
@@ -1,25 +1,19 @@
 class Api::V1::BoatsController < ApplicationController
-
-    # GET /api/v1/users/:user_id/boats
+  # GET /api/v1/users/:user_id/boats
   def index
-    begin
-      boats = Boat.all
-      render json: { status: 'success', data: boats }, status: :ok
-    rescue StandardError => e
-      render json: { status: 'error', message: e.message }, status: :internal_server_error
-    end
+    boats = Boat.all
+    render json: { status: 'success', data: boats }, status: :ok
+  rescue StandardError => e
+    render json: { status: 'error', message: e.message }, status: :internal_server_error
   end
 
   # GET /api/v1/users/:user_id/boats/:id
   def show
-    begin
-      boat = Boat.find(params[:id])
-      render json: { status: 'success', data: boat }, status: :ok
-    rescue ActiveRecord::RecordNotFound
-      render json: { status: 'error', message: 'Boat not found' }, status: :not_found
-    rescue StandardError => e
-      render json: { status: 'error', message: e.message }, status: :internal_server_error
-    end
+    boat = Boat.find(params[:id])
+    render json: { status: 'success', data: boat }, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { status: 'error', message: 'Boat not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { status: 'error', message: e.message }, status: :internal_server_error
   end
-
 end
