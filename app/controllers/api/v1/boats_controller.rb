abdc2d7a989522945a7cc35ac307ae290425ec6c
Ulises <ulisescappa@gmail.com>
2023-12-11 19:38:27 -0300

    Add index method to boats_controller. Add picture column to Boats table. Add picture validation in to boat model.

--- app/controllers/api/v1/boats_controller.rb
+++ app/controllers/api/v1/boats_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::BoatsController < ApplicationController
 
+    def index
+        boats = Boat.all
+        render json: boats
+    end
+
     def show
         boat = Boat.find(params[:id])
         render json: boat
     end
+
 end
