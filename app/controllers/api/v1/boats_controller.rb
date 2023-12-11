df33d7e1d05a23241092433cb7ae356fdc72c206
Ulises <ulisescappa@gmail.com>
2023-12-11 20:25:37 -0300

    Add error handler

--- app/controllers/api/v1/boats_controller.rb
+++ app/controllers/api/v1/boats_controller.rb
@@ -1,13 +1,25 @@
 class Api::V1::BoatsController < ApplicationController
 
-    def index
-        boats = Boat.all
-        render json: boats
+    # GET /api/v1/users/:user_id/boats
+  def index
+    begin
+      boats = Boat.all
+      render json: { status: 'success', data: boats }, status: :ok
+    rescue StandardError => e
+      render json: { status: 'error', message: e.message }, status: :internal_server_error
     end
+  end
 
-    def show
-        boat = Boat.find(params[:id])
-        render json: boat
+  # GET /api/v1/users/:user_id/boats/:id
+  def show
+    begin
+      boat = Boat.find(params[:id])
+      render json: { status: 'success', data: boat }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { status: 'error', message: 'Boat not found' }, status: :not_found
+    rescue StandardError => e
+      render json: { status: 'error', message: e.message }, status: :internal_server_error
     end
+  end
 
 end
