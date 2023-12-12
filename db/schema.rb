45f3ba4b1c2b6d1e4b1d769b3e8944171719795b
felipe <fperezferraro@gmail.com>
2023-12-12 15:01:55 -0300

    Migrated changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_09_202908) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_12_180043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_09_202908) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
