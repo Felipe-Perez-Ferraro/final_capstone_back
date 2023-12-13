4172e2958396c14b798fb061cd503953e958f89f
felipe <fperezferraro@gmail.com>
2023-12-13 17:08:32 -0300

    Generate API documentation for registrations

---
+++ spec/requests/users/registrations_spec.rb
@@ -0,0 +1,53 @@
+require 'swagger_helper'
+
+RSpec.describe 'users/registrations', type: :request do
+
+  path '/signup/sign_up' do
+
+    get('new registration') do
+      tags 'Users Registration'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/signup' do
+    post('create registration') do
+      response(200, 'successful') do
+        tags 'Users Registration'
+        consumes 'application/json'
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                name: { type: :string }
+              },
+              required: [:name]
+            }
+          },
+          required: [:user]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
