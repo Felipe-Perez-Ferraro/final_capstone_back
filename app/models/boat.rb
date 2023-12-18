ef0c2705b27c46bf101c246e7a773d37fcb49bb7
felipe <fperezferraro@gmail.com>
2023-12-18 19:12:49 -0300

    Removed reserved validation from boat model

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -7,6 +7,5 @@ class Boat < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :color, presence: true
   validates :rent_price, presence: true, numericality: { greater_than: 0 }
-  validates :reserved, inclusion: { in: [true, false] }
   validates :user_id, presence: true
 end
